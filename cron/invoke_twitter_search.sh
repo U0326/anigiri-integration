b8783676c5adb149e738640af1f605992aad8026
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2019-02-16 16:18:47 +0900

    クローラを呼び出すシェルを追加した。

---
+++ cron/invoke_twitter_search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+WORK_DIR=${SCRIPT_DIR}/..
+TARGET_PROJECT=anigiri-crawler
+TARGET_FILE=search_twitter_invoker.py
+LOG_FILE=/var/log/anigiri-crawler-shell.log
+
+cd ${WORK_DIR}
+docker-compose run ${TARGET_PROJECT} python ${TARGET_FILE} 2>&1 | tee -a ${LOG_FILE}
