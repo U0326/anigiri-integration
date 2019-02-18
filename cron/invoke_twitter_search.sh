2290a92088d92c98fd6e69924c36272e22d976d5
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2019-02-19 02:50:08 +0900

    クーロン用のシェルの修正を行った。

--- cron/invoke_twitter_search.sh
+++ cron/invoke_twitter_search.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 WORK_DIR=${SCRIPT_DIR}/..
+COMPOSE_PATH=/usr/local/bin/docker-compose
+COMPOSE_FILE=docker-compose.development.yml
 TARGET_PROJECT=anigiri-crawler
 TARGET_FILE=search_twitter_invoker.py
 LOG_FILE=${WORK_DIR}/log/crawler/anigiri-crawler-shell.log
-COMPOSE_FILE=docker-compose.development.yml
 
 export COMPOSE_FILE
 cd ${WORK_DIR}
-docker-compose run ${TARGET_PROJECT} python ${TARGET_FILE} 2>&1 | tee -a ${LOG_FILE}
+${COMPOSE_PATH} run ${TARGET_PROJECT} python ${TARGET_FILE} 2>&1 | tee -a ${LOG_FILE}
 
